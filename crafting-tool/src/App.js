38572793eb2bf730fff80161c09da041e29c1730
Jenn Collins <jenncoll@bu.edu>
2019-07-30 22:59:05 -0400

    Added material UI

--- crafting-tool/src/App.js
+++ crafting-tool/src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import Icon from '@material-ui/core/Icon';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  fab: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Testing
         </p>
         <a
           className="App-link"
@@ -18,6 +33,12 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <Fab color="secondary" aria-label="edit" className={classes.fab}>
+          <Icon>edit_icon</Icon>
+        </Fab>
       </header>
     </div>
   );
